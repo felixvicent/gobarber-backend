89cc9dda3aff8edd989974f22256d80a0583dae3
Felix Vicente <felixvicent1306@gmail.com>
2020-11-16 14:59:36 -0300

    Create model loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Félix Vicente',
+    email: 'felixvicent1306@gmail.com',
+    password_hash: '211235648',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
