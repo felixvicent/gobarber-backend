c86a4776006e09d2e4dbd62355094642ad913478
Felix Vicente <felixvicent1306@gmail.com>
2020-11-16 15:11:47 -0300

    Create store in user

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Félix Vicente',
-    email: 'felixvicent1306@gmail.com',
-    password_hash: '211235648',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
