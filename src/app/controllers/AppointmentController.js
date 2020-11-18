0fa8c19bd68882b08c5b4156f29e570ed0afd92d
Felix Vicente <felixvicent1306@gmail.com>
2020-11-18 13:18:03 -0300

    Add appointments validte

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -11,7 +12,7 @@ class AppointmentController {
     });
 
     if (!(await schema.isValid(req.body)))
-      res.status(400).json({ error: 'Validation fails' });
+      return res.status(400).json({ error: 'Validation fails' });
 
     const { provider_id, date } = req.body;
 
@@ -20,9 +21,23 @@ class AppointmentController {
     });
 
     if (!isProvider)
-      res
+      return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date()))
+      return res.status(400).json({ error: 'Past dates are not permited' });
+
+    const checkAvailability = await Appointment.findOne({
+      where: { provider_id, canceled_at: null, date: hourStart },
+    });
+
+    if (checkAvailability)
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
 
     const appointment = await Appointment.create({
       user_id: req.userId,
@@ -30,7 +45,7 @@ class AppointmentController {
       date,
     });
 
-    return res.json(appointment);
+    return res.status(200).json(appointment);
   }
 }
 
