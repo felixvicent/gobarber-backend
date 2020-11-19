4a59c9eaae8219d706f4098b81614c6de8c4e057
Felix Vicente <felixvicent1306@gmail.com>
2020-11-19 17:13:06 -0300

    Config nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'fe56c6582ea950',
+    pass: 'ba1c592b5ff409',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
