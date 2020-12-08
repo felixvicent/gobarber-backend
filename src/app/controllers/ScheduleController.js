80931844d51578c9c9a6d2632041b8140d3811d0
Felix Vicente <felixvicent1306@gmail.com>
2020-12-08 14:56:38 -0300

    Small fixes

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -23,6 +23,7 @@ class ScheduleController {
         canceled_at: null,
         date: { [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)] },
       },
+      include: [{ model: User, as: 'user', attributes: ['name'] }],
       order: ['date'],
     });
 
